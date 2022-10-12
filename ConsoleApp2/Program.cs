af2232a1c0d6bc522f32ac29c5e4ce6327d48eb2
Danielzhel <fartunazh@gmail.com>
2022-10-13 00:49:55 +0300

    Fixed some punctuation errors

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,43 +7,42 @@ while (true)
 
     try // checking data type
     {
-        Console.WriteLine("Enter first number");
+        Console.WriteLine("Enter first number.");
         a = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Enter second number");
+        Console.WriteLine("Enter second number.");
         b = Convert.ToDouble(Console.ReadLine());
     }
     catch (Exception)
     {
-        Console.WriteLine("No valid numder. Try again!");
+        Console.WriteLine("No valid number. Try again!");
         Console.ReadLine();
         continue;
     }
-    
 
-    Console.WriteLine("Choose on of the action: sub, mult, div, add");
+    Console.WriteLine("Choose on of the action: sub, mult, div, add.");
     act = Console.ReadLine();
 
     switch (act)
     {
         case "sub":
-            Console.WriteLine($"Result {a - b}");
+            Console.WriteLine($"Result: {a - b}");
             break;
         case "add":
-            Console.WriteLine($"Result {a + b}");
+            Console.WriteLine($"Result: {a + b}");
             break;
         case "div":
             if (b == 0)
             {
-                Console.WriteLine("Can`t be divided by 0. Try again"); //prevent division by 0
+                Console.WriteLine("Can`t be divided by 0. Try again!"); //prevent division by 0
             }
-            Console.WriteLine($"Result {a / b}");
+            Console.WriteLine($"Result: {a / b}");
             break;
         case "mult":
             Console.WriteLine($"Result: {a * b}");
             break;
         default:
-            Console.WriteLine("No valid action. Try again");
+            Console.WriteLine("No valid action. Try again!");
             break;
     }
     Console.ReadLine();
