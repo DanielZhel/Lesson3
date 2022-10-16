a3b733d99969bc76cc81345a32a2bf896027fe80
Danielzhel <fartunazh@gmail.com>
2022-10-16 11:19:36 +0300

    Added methods

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,67 +1,41 @@
-﻿Console.Title = "Calculator";
+﻿using enteringValues;
 
-string act,c;
-double a, b;
+Console.Title = "Calculator";
 
-c = "+";
+double a = 0;
+double b = 0;
 
-double[] fiveREZ = new double[5];
+string act ="", c = "+", r = "+";
+
+
 
 while (c == "+")
 {
     Console.Clear();
+    Input.Num();
+    Input.actions();
+    
 
-    try // checking data type
+    Console.WriteLine("Do you want to see the last five results?");
+    r = Console.ReadLine();
+    if(r == "+")
     {
-        Console.WriteLine("Enter first number.");
-        a = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Enter second number.");
-        b = Convert.ToDouble(Console.ReadLine());
     }
-    catch (Exception)
+    else
     {
-        Console.WriteLine("No valid number. Try again!");
-        Console.ReadLine();
         continue;
     }
-
-    Console.WriteLine("Choose on of the action: sub, mult, div, add.");
-    act = Console.ReadLine();
-
-    switch (act)
-    {
-        case "sub":
-            Console.WriteLine($"Result: {a - b}");
-            break;
-        case "add":
-            Console.WriteLine($"Result: {a + b}");
-            break;
-        case "div":
-            if (b == 0)
-            {
-                Console.WriteLine("Can`t be divided by 0. Try again!"); //prevent division by 0
-            }
-            Console.WriteLine($"Result: {a / b}");
-            break;
-        case "mult":
-            Console.WriteLine($"Result: {a * b}");
-            break;
-        default:
-            Console.WriteLine("No valid action. Try again!");
-            break;
-    }
-
-    Console.WriteLine("If you want to continue calculations PRESS +, otherwise PRESS -!");
+    
+    Console.WriteLine("Do you want to continue?(+/-)");
     c = Console.ReadLine();
-
-        if (c == "+" )
-        {
-            c = "+";
-        }
 
 }
 
+
+    
+
+   
 
     
     
