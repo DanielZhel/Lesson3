ca3e7cd9a6b717370243659e25dcb1052d3340c6
DanielZhel <114695564+DanielZhel@users.noreply.github.com>
2022-10-16 11:21:26 +0300

    Revert "Added methods"

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,41 +1,67 @@
-﻿using enteringValues;
+﻿Console.Title = "Calculator";
 
-Console.Title = "Calculator";
+string act,c;
+double a, b;
 
-double a = 0;
-double b = 0;
+c = "+";
 
-string act ="", c = "+", r = "+";
-
-
+double[] fiveREZ = new double[5];
 
 while (c == "+")
 {
     Console.Clear();
-    Input.Num();
-    Input.actions();
-    
 
-    Console.WriteLine("Do you want to see the last five results?");
-    r = Console.ReadLine();
-    if(r == "+")
+    try // checking data type
     {
+        Console.WriteLine("Enter first number.");
+        a = Convert.ToDouble(Console.ReadLine());
 
+        Console.WriteLine("Enter second number.");
+        b = Convert.ToDouble(Console.ReadLine());
     }
-    else
+    catch (Exception)
     {
+        Console.WriteLine("No valid number. Try again!");
+        Console.ReadLine();
         continue;
     }
-    
-    Console.WriteLine("Do you want to continue?(+/-)");
+
+    Console.WriteLine("Choose on of the action: sub, mult, div, add.");
+    act = Console.ReadLine();
+
+    switch (act)
+    {
+        case "sub":
+            Console.WriteLine($"Result: {a - b}");
+            break;
+        case "add":
+            Console.WriteLine($"Result: {a + b}");
+            break;
+        case "div":
+            if (b == 0)
+            {
+                Console.WriteLine("Can`t be divided by 0. Try again!"); //prevent division by 0
+            }
+            Console.WriteLine($"Result: {a / b}");
+            break;
+        case "mult":
+            Console.WriteLine($"Result: {a * b}");
+            break;
+        default:
+            Console.WriteLine("No valid action. Try again!");
+            break;
+    }
+
+    Console.WriteLine("If you want to continue calculations PRESS +, otherwise PRESS -!");
     c = Console.ReadLine();
+
+        if (c == "+" )
+        {
+            c = "+";
+        }
 
 }
 
-
-    
-
-   
 
     
     
